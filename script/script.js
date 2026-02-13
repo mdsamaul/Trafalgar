39625d714dc4d11e2f1daa9c72986a5e5804c5de
mdsamaul <mdsamaul843@gmail.com>
2026-02-13 12:00:17 +0600

    responsive

--- script/script.js
+++ script/script.js
@@ -1,1 +1,6 @@
-console.log("pranto");
+const hamburger = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".headRightnav");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
